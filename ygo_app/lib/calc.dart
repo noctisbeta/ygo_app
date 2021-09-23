ec9becec5ccfa993e4ea8978747163a6bb89431d
NoctisBeta <noctisbeta@gmail.com>
2021-09-23 05:52:49 +0200

    scaffold for new page

--- ygo_app/lib/calc.dart
+++ ygo_app/lib/calc.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'entry_adding_page.dart';
 // import 'package:uuid/uuid.dart';
+import 'small_world.dart';
 
 class CalcPage extends StatefulWidget {
   const CalcPage({Key? key}) : super(key: key);
@@ -25,6 +26,31 @@ class _CalcPageState extends State<CalcPage> {
         width: 100,
         child: Drawer(
           child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Ink(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Theme.of(context).accentColor, width: 4.0),
+                  shape: BoxShape.circle,
+                ),
+                child: InkWell(
+                  focusColor: Colors.red,
+                  splashColor: Theme.of(context).accentColor,
+                  highlightColor: Theme.of(context).backgroundColor,
+                  borderRadius: BorderRadius.circular(10.0),
+                  onTap: () => Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (_) => SmallWorldPage())),
+                  child: const Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Icon(
+                      Icons.auto_graph,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.all(10),
               child: Ink(
---
+++ ygo_app/lib/small_world.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class SmallWorldPage extends StatefulWidget {
+  const SmallWorldPage({Key? key}) : super(key: key);
+
+  @override
+  _SmallWorldPageState createState() => _SmallWorldPageState();
+}
+
+class _SmallWorldPageState extends State<SmallWorldPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: SizedBox(
+        width: 100,
+        child: Drawer(
+          child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Ink(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Theme.of(context).accentColor, width: 4.0),
+                  shape: BoxShape.circle,
+                ),
+                child: InkWell(
+                  focusColor: Colors.red,
+                  splashColor: Theme.of(context).accentColor,
+                  highlightColor: Theme.of(context).backgroundColor,
+                  borderRadius: BorderRadius.circular(10.0),
+                  onTap: () => Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (_) => SmallWorldPage())),
+                  child: const Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Icon(
+                      Icons.home,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Ink(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Theme.of(context).accentColor, width: 4.0),
+                  shape: BoxShape.circle,
+                ),
+                child: InkWell(
+                  focusColor: Colors.red,
+                  splashColor: Theme.of(context).accentColor,
+                  highlightColor: Theme.of(context).backgroundColor,
+                  borderRadius: BorderRadius.circular(10.0),
+                  onTap: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Icon(
+                      Icons.dark_mode_outlined,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Ink(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Theme.of(context).accentColor, width: 4.0),
+                  shape: BoxShape.circle,
+                ),
+                child: InkWell(
+                  focusColor: Colors.red,
+                  splashColor: Theme.of(context).accentColor,
+                  highlightColor: Theme.of(context).backgroundColor,
+                  borderRadius: BorderRadius.circular(10.0),
+                  onTap: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Icon(
+                      Icons.info_outline,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ),
+      appBar: AppBar(
+        title: const Text('Small World'),
+      ),
+    );
+  }
+}
